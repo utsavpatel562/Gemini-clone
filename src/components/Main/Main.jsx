e13259e2c3b739ddde1074303d82831429dffefe
Utsav Patel ✨ <158522755+utsavpatel562@users.noreply.github.com>
2024-05-12 19:06:14 -0400

    Add files via upload

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,107 @@
+import "./Main.css";
+import { assets } from "../../assets/assets";
+import { useContext } from "react";
+import { Context } from "../../context/Context";
+
+const Main = () => {
+  // destructuring the object of context
+  const {
+    input,
+    setInput,
+    onSent,
+    recentPrompt,
+    showResult,
+    loading,
+    resultData,
+  } = useContext(Context);
+  return (
+    <>
+      <div className="main">
+        <div className="nav">
+          <p>Gemini</p>
+          <img src={assets.user_icon} alt="UserIcon" />
+        </div>
+        <div className="main-container">
+          {!showResult ? (
+            <>
+              <div className="greet">
+                <p>
+                  <span>Hello, Utsav</span>
+                </p>
+                <p>How can I help you today?</p>
+              </div>
+              <div className="cards">
+                <div className="card">
+                  <p>Suggest beautiful places to see in Canada</p>
+                  <img src={assets.compass_icon} alt="CompassIcon" />
+                </div>
+                <div className="card">
+                  <p>Plan a low-carb meal with what's available in my fridge</p>
+                  <img src={assets.bulb_icon} alt="CompassIcon" />
+                </div>
+                <div className="card">
+                  <p>List power words for my resume that show teamwork</p>
+                  <img src={assets.message_icon} alt="CompassIcon" />
+                </div>
+                <div className="card">
+                  <p>Create a list of power phrases for my resume</p>
+                  <img src={assets.code_icon} alt="CompassIcon" />
+                </div>
+              </div>
+            </>
+          ) : (
+            <div className="result">
+              <div className="result-title">
+                <img src={assets.user_icon} alt="UserIcon" />
+                <p>{recentPrompt}</p>
+              </div>
+              <div className="result-data">
+                <img src={assets.gemini_icon} alt="GeminiIcon" />
+                {loading ? (
+                  <div className="loader">
+                    <hr />
+                    <hr />
+                    <hr />
+                  </div>
+                ) : (
+                  <p dangerouslySetInnerHTML={{ __html: resultData }}></p>
+                )}
+              </div>
+            </div>
+          )}
+
+          <div className="main-bottom">
+            <div className="search-box">
+              <input
+                onChange={(event) => setInput(event.target.value)}
+                value={input}
+                type="text"
+                placeholder="Enter a prompt here"
+              />
+              <div className="search-box-icon">
+                <img src={assets.gallery_icon} alt="GalleryIcon" />
+                <img src={assets.mic_icon} alt="MicIcon" />
+                {input ? (
+                  <img
+                    onClick={() => onSent()}
+                    src={assets.send_icon}
+                    alt="SendIcon"
+                  />
+                ) : null}
+              </div>
+            </div>
+            <p className="bottom-info">
+              Gemini may display inaccurate info, including about people, so
+              double-check its responses.{" "}
+              <a href="https://support.google.com/gemini/answer/13594961?visit_id=638488069169109558-2959892032&p=privacy_notice&rd=1#privacy_notice">
+                Your privacy & Gemini Apps
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Main;
---
+++ src/context/Context.jsx
@@ -0,0 +1,76 @@
+import { createContext, useState } from "react";
+import runChat from "../config/gemini";
+
+export const Context = createContext();
+
+const ContextProvider = (props) => {
+  const [input, setInput] = useState("");
+  const [recentPrompt, setRecentPrompt] = useState("");
+  const [prevPrompts, setPrevPrompts] = useState([]);
+  const [showResult, setShowResult] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [resultData, setResultData] = useState("");
+
+  const delayPara = (index, nextWord) => {
+    setTimeout(function () {
+      setResultData((prev) => prev + nextWord);
+    }, 75 * index);
+  };
+
+  const newChat = () => {
+    setLoading(false);
+    setShowResult(false);
+  };
+
+  const onSent = async (prompt) => {
+    setResultData("");
+    setLoading(true);
+    setShowResult(true);
+    let response;
+    if (prompt !== undefined) {
+      response = await runChat(prompt);
+      setRecentPrompt(prompt);
+    } else {
+      setPrevPrompts((prev) => [...prev, input]);
+      setRecentPrompt(input);
+      response = await runChat(input);
+    }
+
+    let responseArray = response.split("**");
+    let newResponse = "";
+    for (let i = 0; i < responseArray.length; i++) {
+      if (i == 0 || i % 2 !== 1) {
+        newResponse += responseArray[i];
+      } else {
+        newResponse += "<b>" + responseArray[i] + "</b>";
+      }
+    }
+    let newResponse2 = newResponse.split("*").join("</br>");
+    let newResponseArray = newResponse2.split(" ");
+    for (let i = 0; i < newResponseArray.length; i++) {
+      const nextWord = newResponseArray[i];
+      delayPara(i, nextWord + " ");
+    }
+    setLoading(false);
+    setInput("");
+  };
+
+  const contextValue = {
+    prevPrompts,
+    setPrevPrompts,
+    onSent,
+    recentPrompt,
+    setRecentPrompt,
+    showResult,
+    loading,
+    resultData,
+    input,
+    setInput,
+    newChat,
+  };
+  return (
+    <Context.Provider value={contextValue}>{props.children}</Context.Provider>
+  );
+};
+
+export default ContextProvider;
